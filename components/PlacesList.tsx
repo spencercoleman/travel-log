6863615156c19a916397b0fedff3262d6c5bf799
spencercoleman <spencer@edc.vc>
2022-10-30 23:58:50 -0700

    Add initial filters for places list

--- components/PlacesList.tsx
+++ components/PlacesList.tsx
@@ -3,16 +3,12 @@ import { TypePlace } from '../types';
 import PlaceCard from './PlaceCard';
 
 interface Props {
-    title: string;
     places: TypePlace[];
 }
 
-const PlacesList = ({ title, places }: Props): JSX.Element => {
+const PlacesList = ({ places }: Props): JSX.Element => {
     return (
         <Stack spacing={3}>
-            <Heading as="h2" size="lg">
-                {title}
-            </Heading>
             <SimpleGrid columns={[1, null, 2, 3]} spacing="20px">
                 {places.map((place) => (
                     <PlaceCard key={place.sys.id} place={place} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
+import { useState, useEffect } from 'react';
 import { InferGetStaticPropsType, GetStaticProps } from 'next';
+import { Heading, Button, Flex, Select, Stack } from '@chakra-ui/react';
 import { TypePlace, TypePlaceFields } from '../types';
 import { createClient } from 'contentful';
 import PlacesList from '../components/PlacesList';
@@ -24,9 +26,96 @@ export const getStaticProps: GetStaticProps<{
 };
 
 const Home = ({ places }: InferGetStaticPropsType<typeof getStaticProps>) => {
+    const [filteredPlaces, setFilteredPlaces] = useState<TypePlace[]>([]);
+    const [placeTypeFilter, setPlaceTypeFilter] = useState<string>('');
+    const [temperatureFilter, setTemperatureFilter] = useState<string>('');
+    const [distanceFilter, setDistanceFilter] = useState<string>('');
+
+    const placeTypes = ['Beach', 'City', 'Camp', 'Mountains'];
+    const temperatures = ['Cold', 'Temperate', 'Hot'];
+    const distances = ['Close', 'Mid', 'Far'];
+
+    const clearFilters = (): void => {
+        setPlaceTypeFilter('');
+        setTemperatureFilter('');
+        setDistanceFilter('');
+    };
+
+    useEffect(() => {
+        const filterPlaces = () => {
+            return places.filter((place) => {
+                const { type, temperature, distance } = place.fields;
+
+                return (
+                    type.includes(placeTypeFilter) &&
+                    temperature.includes(temperatureFilter) &&
+                    distance.includes(distanceFilter)
+                );
+            });
+        };
+
+        const filteredPlaces = filterPlaces();
+        setFilteredPlaces(filteredPlaces);
+    }, [places, placeTypeFilter, temperatureFilter, distanceFilter]);
+
     return (
         <section>
-            <PlacesList title={'Places to go'} places={places} />
+            <Stack spacing={3}>
+                <Heading as="h2" size="lg">
+                    Places to go
+                </Heading>
+
+                <Flex gap={2}>
+                    <Select
+                        placeholder="Place type"
+                        value={placeTypeFilter}
+                        onChange={(e) => setPlaceTypeFilter(e.target.value)}
+                    >
+                        {placeTypes.map((placeType) => (
+                            <option
+                                key={placeType}
+                                value={placeType.toLowerCase()}
+                            >
+                                {placeType}
+                            </option>
+                        ))}
+                    </Select>
+                    <Select
+                        placeholder="Temperature"
+                        value={temperatureFilter}
+                        onChange={(e) => setTemperatureFilter(e.target.value)}
+                    >
+                        {temperatures.map((temperature) => (
+                            <option
+                                key={temperature}
+                                value={temperature.toLowerCase()}
+                            >
+                                {temperature}
+                            </option>
+                        ))}
+                    </Select>
+                    <Select
+                        placeholder="Distance"
+                        value={distanceFilter}
+                        onChange={(e) => setDistanceFilter(e.target.value)}
+                    >
+                        {distances.map((distance) => (
+                            <option
+                                key={distance}
+                                value={distance.toLowerCase()}
+                            >
+                                {distance}
+                            </option>
+                        ))}
+                    </Select>
+
+                    <Button minW="fit-content" onClick={clearFilters}>
+                        Clear Filters
+                    </Button>
+                </Flex>
+
+                <PlacesList places={filteredPlaces} />
+            </Stack>
         </section>
     );
 };
